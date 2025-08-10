0b3dd97e6215b80a52e0c82c9420cc085df94a44
Magomed Arsaev <magomed.arsaev@coapp.io>
2025-08-10 11:54:51 +0200

    feat: add art gallery feature
    - new art item
    - art filter by medium (oil, watercolor)
    - softbox

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,18 +2,31 @@ import { defineCollection, z } from 'astro:content'
 
 const blog = defineCollection({
     type: 'content',
-    // Type-check frontmatter using a schema
     schema: z.object({
         title: z.string(),
         description: z.string(),
-        // Transform string to Date object
         pubDate: z.coerce.date(),
         updatedDate: z.coerce.date().optional(),
         heroImage: z.string().optional(),
         tags: z.array(z.string()).optional(),
     }),
-})
+});
+
+const art = defineCollection({
+    type: 'content',
+    schema: z.object({
+        name: z.string(),
+        medium: z.string(),
+        dimensions: z.string(),
+        year: z.number(),
+        month: z.number(),
+        heroImage: z.string(),
+        author: z.string(),
+        text: z.string(),
+        status: z.string().optional(), // Sold/Available
+    }),
+});
 
 
-export const collections = { blog }
+export const collections = { blog, art }
 
